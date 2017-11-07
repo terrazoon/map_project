9e3cd319c1eeaba7e6bb6d782dcc1f7662fcafd9
VolpeAzzurro <terrazoon@gmail.com>
2017-11-07 04:06:31 +0000

    Add README and LICENSE

---
+++ README.md
@@ -0,0 +1,16 @@
+# map project
+This is application provides some fascinating details about sites in the city of Santa Clarita.
+ 
+
+
+# version
+version 0.1
+
+# authors
+Kenneth Kehl
+
+# acknowledgements
+This project was a class project for the Udacity full stack nanodegree.
+
+# license
+This project is released under the <a href="https://opensource.org/licenses/MIT">MIT License</a>
--- js/app.js
+++ js/app.js
@@ -1,44 +1,4 @@
 
-//var initialCats = [
-
-
-//        {
-//            clickCount : 0,
-//            name : 'Tabby',
-//            imgSrc : 'fatcat.jpg',
-//            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
-//            nicknames: ['JavaScript','KnockoutJS','BackboneJS','EmberJS']
-    
-//        },
-//        {
-//            clickCount : 0,
-//            name : 'Tiger',
-//            imgSrc : 'russian.jpg',
-//            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
-//            nicknames: ['Rolf']
-//        }
-//]
-//var Cat = function(data) {
-
-//    this.clickCount = ko.observable(data.clickCount);
-//    this.name = ko.observable(data.name);
-    
-//    this.imgSrc = ko.observable(data.imgSrc);
-//    this.imgAttribution = ko.observable(data.imgAttribution);
-//    this.nicknames = ko.observableArray(data.nicknames);
-
-    
-    
-//    this.level = ko.computed(function () {
-//        if (this.clickCount() < 10) return 'Newborn';
-//        if (this.clickCount() < 20) return 'kitten';
-//        return 'grandmaster';
-//    }, this);
-    
-        
-//}
-
-//TODO remove
 var locations = [
     {title: 'Vista Valencia Golf Course', location: {lat: 34.3857, lng: -118.5642}},
     {title: 'California Institute of the Arts', location: {lat: 34.3933, lng: -118.5668}},
@@ -55,6 +15,7 @@ var Location = function(title, location) {
 
 let API_KEY = '7eb2739c11560e56f77b4edae27e19b2';
 
+//non-google third party api
 function getWeather(latitude, longtitude) {
   $.ajax({
     url: 'http://api.openweathermap.org/data/2.5/weather',
@@ -92,35 +53,11 @@ var ViewModel = function() {
     this.durationList = ko.observableArray(["10", "15", "30", "60"]);
     this.modeList = ko.observableArray(["DRIVING", "WALKING", "BICYCLING", "TRANSIT"]);   
     
-    
-    //this.weather = ko.observable(new Weather().summary);
-    
-    //this.catList = ko.observableArray([]);
-
-    //initialCats.forEach(function(catItem){
-    //    self.catList.push( new Cat(catItem) );
-    //});
-    
-    //this.currentCat = ko.observable(this.catList()[0]);
-    
     this.getWeather = function() {
         //$.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=5393049&APPID=7eb2739c11560e56f77b4edae27e19b2",function(json){
         //self.weather =  JSON.stringify(json);
         self.weather = "weather created";
     }   
-
-
-    
-    //this.incrementCounter = function() {
-    //    this.clickCount(this.clickCount() + 1);
-    
-    //};
-    
-    //this.setCat = function(clickedCat) {
-    //    self.currentCat(clickedCat);
-    //}
-    
-   
     
 }
 
