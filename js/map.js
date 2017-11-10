471682dbe4de0ab344e7fc8a4bf0af6d1ad243b6
VolpeAzzurro <terrazoon@gmail.com>
2017-11-10 20:25:09 +0000

    adjust zoom

--- js/map.js
+++ js/map.js
@@ -372,7 +372,7 @@
             }, function(results, status) {
               if (status == google.maps.GeocoderStatus.OK) {
                 map.setCenter(results[0].geometry.location);
-                map.setZoom(12);
+                map.setZoom(18);
               } else {
                 window.alert('We could not find that location - try entering a more' +
                     ' specific place.');
